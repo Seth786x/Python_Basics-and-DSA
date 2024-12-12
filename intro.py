9022552e3e55a44d36e88abe698212b372434501
Seth786x <spark786rocks@gmail.com>
2024-12-13 01:08:46 +0530

    Add files via upload
    These are some basic things while starting python

---
+++ intro.py
@@ -0,0 +1,26 @@
+
+
+print("hello")
+
+user_name = input("enter your name")
+print(user_name)
+print("that's a great name !")
+
+x = 5
+print("The value of x is ", x)
+y = int(input("Enter the value of y"))
+print("The addition is ", (x+y))
+
+if x+y <=10:
+    print("The number is less than or equal to 10")
+else:
+    print("The number is greater than 10")
+
+for i in range(0, 9):
+    print(i)
+
+i=9
+while i>0:
+    print(i)
+    i = i-1
+
