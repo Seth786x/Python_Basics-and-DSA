ea5289bf61136e3e87045233d793b6f5a4ef7eca
Seth786x <spark786rocks@gmail.com>
2024-12-13 01:15:45 +0530

    Update intro.py
    This file contains python concepts like input/output, variables, arithmetic operations, conditionals, and loops.

--- intro.py
+++ intro.py
@@ -1,21 +1,25 @@
 
-
+#Display values
 print("hello")
 
+#taking things as input
 user_name = input("enter your name")
 print(user_name)
 print("that's a great name !")
 
+#can perform arithmetic operations
 x = 5
 print("The value of x is ", x)
 y = int(input("Enter the value of y"))
 print("The addition is ", (x+y))
 
+#conditional statements
 if x+y <=10:
     print("The number is less than or equal to 10")
 else:
     print("The number is greater than 10")
 
+#loops
 for i in range(0, 9):
     print(i)
 
