57914bd794993cafa3b619c4d484133e74a99742
Seth786x <spark786rocks@gmail.com>
2024-12-13 19:58:27 +0530

    Add files via upload
    This is the second file, includes ways we can take an input in python. 
    Remove the comments to run the specific part.

---
+++ Input.py
@@ -0,0 +1,25 @@
+# # name = input()
+# meow = int(input())
+# b = bool(input("give the boolean input"))
+# f = float(input("enter the float input"))
+
+# print(type(name))
+# print(type(meow))
+# print(type(b))
+# print(type(f))
+
+
+#taking multiple numbers as input
+
+# x, y, z = map(int, input("enter the three numbers").split())
+# # print(x, y, z)
+# print((x))
+# print((y))
+# print((z))
+
+#Note-  We cannot create multiple variables for n no of elements, hence we use lists
+#below is a small example of list 
+
+# xx = list(map(int, input().split()))
+# print(xx)
+# print(type(xx))
