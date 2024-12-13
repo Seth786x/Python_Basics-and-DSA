2c24ea8a63ae1706390529e543f3305587962b8a
Seth786x <spark786rocks@gmail.com>
2024-12-13 22:20:48 +0530

    Add files via upload
    This is the third file of python
    Incorporated with different datatypes, this will help new python users to get more famimilar with the different ways we can store data
    Understanding these data types and structures is crucial for working with data in Python.

---
+++ Datatypes.py
@@ -0,0 +1,27 @@
+name = "seth"
+age = 21
+gender = "Male"
+weight = 64.2
+isStrong = True
+
+hobbies = {"Computer hardware tinkering", "Trading", "working out", "Gaming", "reading manhwa"}
+
+FinancialTargets2025 = [20000, 30000, 45000, "1L"]
+EarningBefore2022 = (-12000, -20000, -50000, "-4L")
+
+PcPeripherals = {
+    "monitor " : "Acer KG series",
+    "GPU":"RTX 2060",
+    "mouse":"Razer Deathadder",
+    "ram" : 16
+}
+
+print(type(name))
+print(type(age))
+print(type(gender))
+print(type(weight))
+print(type(isStrong))
+print(type(hobbies))
+print(type(FinancialTargets2025))
+print(type(EarningBefore2022))
+print(type(PcPeripherals))
