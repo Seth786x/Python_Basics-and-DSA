7147886e8616352fcf66489a7a19a77a69c7b3fa
Seth786x <spark786rocks@gmail.com>
2024-12-13 23:39:53 +0530

    Add files via upload
    This is the 4th file, an addition of two strings, and some basic operations of lists can be found in the program. Tuples are not mutable so the only option to get the elements.
    The program is really easy to understand and good for beginners

---
+++ StringListTuple.py
@@ -0,0 +1,28 @@
+first = "Seth"
+last = " ken"
+
+name = first + last
+print(name)
+
+lst = [1, 2, 3, 4]
+
+lst.append(5);
+print(lst)
+lst.pop(3)
+print(lst)
+
+lst.insert(1, 3)
+print(lst)
+
+lst.remove(5)
+print(lst)
+
+for i  in lst:
+    print(i)
+
+print("------------------------------")
+
+num = (3, 4, 5, 6, 12)
+
+for i in num:
+    print(i)
