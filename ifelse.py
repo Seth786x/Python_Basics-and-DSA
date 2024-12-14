6527588d9a5a36ecee3152f423449cea5cca28a1
Seth786x <spark786rocks@gmail.com>
2024-12-14 19:06:00 +0530

    Add files via upload
    If else is too simple guys, I know it won't take much to know about it 
    keep going !

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+working_hours = int(input(" Enter the number of hours you have worked"))
+
+if (working_hours == 4):
+    print("keep going!")
+
+elif(working_hours >= 3):
+    print("need to work harder")
+
+else:
+    print("are you even working?")
